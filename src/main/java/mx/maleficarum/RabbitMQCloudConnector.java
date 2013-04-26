41b6d3379abf9d237a74524bd4c4c2948688c24d
maleficarum <o@maleficarum.mx>
2013-04-26 10:52:14 -0500

    Added base functions for connecting to rabbitmq

--- src/main/java/mx/maleficarum/RabbitMQCloudConnector.java
+++ src/main/java/mx/maleficarum/RabbitMQCloudConnector.java
@@ -3,12 +3,24 @@
  */
 package mx.maleficarum;
 
+import java.io.IOException;
+
+import org.mule.api.annotations.Connect;
+import org.mule.api.annotations.Disconnect;
 import org.mule.tools.cloudconnect.annotations.Connector;
 import org.mule.tools.cloudconnect.annotations.Operation;
 import org.mule.tools.cloudconnect.annotations.Property;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 @Connector(namespacePrefix="rabbitmq")
 public class RabbitMQCloudConnector {
+	
+
+	private Connection connection = null;
+	protected Channel channel = null;
 	
 	@Property
 	private String queueName;
@@ -73,6 +85,30 @@ public class RabbitMQCloudConnector {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	@Disconnect
+	public void disconnect() throws Exception {
+		channel.close();
+		connection.close();		
+	}
+	
+	@Connect
+	public void connect() {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(host);
+		factory.setUsername(username);
+		factory.setPassword(password);
+		
+
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+
+			channel.queueDeclare(queueName, false, false, false, null);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}		
 	}
 
 
